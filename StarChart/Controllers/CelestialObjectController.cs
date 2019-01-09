1ae9098e093898234b6e79756a26f0ea47315ff9
Broderick Daniel <broderickdaniel0@gmail.com>
2019-01-09 13:48:18 -0800

    Completed the module

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -72,5 +73,74 @@ namespace StarChart.Controllers
             }
             return Ok(celestialObjects);
         }
+
+
+        [HttpPost]
+
+        // Return type that accepts parameter [FromBody]CelestialObject...
+        public IActionResult Create([FromBody]CelestialObject celestialObject)
+        {
+            //Adds to data to the database...
+            _context.CelestialObjects.Add(celestialObject);
+            _context.SaveChanges();
+            //Adds new object with a new Id to the database...
+            return CreatedAtRoute("GetById", new { id = celestialObject.Id }, celestialObject);
+        }
+
+        // Passes the parameters
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, CelestialObject celestialObject
+        {
+            //Searches for an existing object by id (Find)...
+            var existingObject = _context.CelestialObjects.Find(id);
+            if (existingObject == null)
+                return NotFound();
+
+            // If found then the name, period, and id are all updated...
+            existingObject.Name = celestialObject.Name;
+            existingObject.OrbitalPeriod = celestialObject.OrbitalPeriod;
+            existingObject.Id = celestialObject.Id;
+
+            // Updates and saves the changes made...
+            _context.CelestialObjects.Update(existingObject);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        // Specifies the which areas that are needed to be fixed...
+        [HttpPatch("{id}/{name}")]
+        public IActionResult RenameObject(int id, string name)
+        {
+            // Searches for existing object by id (Find)...
+            var existingObject = _context.CelestialObjects.Find(id);
+            if (existingObject == null)
+                return NotFound();
+
+            // Only updates the name...(as specified in the parameters)...
+            existingObject.Name = name;
+            _context.CelestialObjects.Update(existingObject);
+            _context.SaveChanges();
+            return NoContent();
+
+
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            //Gets entire Celestial Object listings who's id's matches the parameters in the method...
+            var celestialObjects = _context.CelestialObjects.Where(e => e.Id == id || e.OrbitedObjectId == id);
+
+            // Looks if there are any matches in the database...
+            if (!celestialObjects.Any())
+                return NotFound();
+
+            //Deletes that entry and saves the changes...
+            _context.CelestialObjects.RemoveRange(celestialObjects);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
     }
 }
